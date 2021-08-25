236768d7de0d6fdcbc3194d81eeab3d07306ff4b
dada <>
2021-08-25 18:10:04 +0800

    UtilResponse修正

--- app/Util/UtilResponse.php
+++ app/Util/UtilResponse.php
@@ -1,17 +1,26 @@
 <?php
 namespace App\Util;
+use Illuminate\Http\JsonResponse;
+
 class UtilResponse{
     /**
      * 客製化Json回傳
      * @parameter $status: Bool, $message: String, $data: Any
-     * @return \Illuminate\Http\JsonResponse
-    **/
-    public static function toJson($status = true, $message = "", $data = []): \Illuminate\Http\JsonResponse {
-        return response()->json(
-            [
-                "status" => $status,
-                "message" => $message,
-                "data" => $data,
-            ]);
+     * @param bool $status
+     * @param string $message
+     * @param array $data
+     * @return JsonResponse
+     */
+    public static function toJson(bool $status = true, string $message = "", array $data = []): JsonResponse {
+        $statusCode = $status ? 200 : 400;
+        $response = [
+            "status_code" => $statusCode,
+            "message" => $message,
+        ];
+        if (!empty($data)) {
+            $response["data"] = $data;
+        }
+
+        return response()->json($response, $statusCode);
     }
 }
