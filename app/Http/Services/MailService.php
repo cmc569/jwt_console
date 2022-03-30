bab7ff57c50d9162fde0e21b86a2e1f859d1853a
Jason <jason.chen@accuhit.net>
2022-03-30 16:12:08 +0800

    modify mail service using phpmailer

--- app/Http/Services/MailService.php
+++ app/Http/Services/MailService.php
@@ -2,20 +2,37 @@
 
 namespace App\Http\Services;
 
-use Illuminate\Support\Facades\Mail;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 class MailService {
 
     public static function send(String $to, String $subject, String $content, String $attach=null, String $from='info@accuhit.net')
     {
-        return Mail::raw($content, function ($message) use ($to, $subject, $content, $attach, $from) {
-            $message->from($from);
-            $message->to($to);
-            $message->subject($subject);
+        try {
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();
+            $mail->Host = 'smtp.office365.com';
+            $mail->Port = 587;
+            $mail->SMTPSecure = 'tls';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'project@accunix.com.tw';
+            $mail->Password = 'Qop73620';
+            $mail->From = "project@accunix.com.tw";
+            $mail->AddAddress($to);
+            $mail->IsHTML (true);
+            $mail->Subject = $subject;
+            $mail->Body = nl2br($content);
+            $mail->AltBody = "text/html";
 
-            if (!empty($attach)) {
-                $message->attach($attach);
+            if ($attach) {
+                $mail->addAttachment($attach); 
             }
-        });
+
+            // print_r($mail->Send());
+            echo "Message has been sent\n";
+        }catch (Exception $exception){
+            echo $exception->getMessage()."\n";
+        }
     }
 }
