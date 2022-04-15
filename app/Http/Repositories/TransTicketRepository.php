6b74f065641685c18d973df82418abcca5957965
Jason <jason.chen@accuhit.net>
2022-04-15 17:39:25 +0800

    trans ticket repo modify

--- app/Http/Repositories/TransTicketRepository.php
+++ app/Http/Repositories/TransTicketRepository.php
@@ -3,16 +3,18 @@
 namespace App\Http\Repositories;
 
 use App\Crypto\Crypto;
-// use App\Http\Models\AccunixCoupons;
+use App\Http\Models\AccunixCoupons;
 use App\Http\Models\AccunixCouponChildLogs;
+use App\Http\Models\ProjectBurgerkingCoupons;
+use App\Http\Models\ProjectBurgerkingCouponChildLogs;
 use App\Http\Models\Roles;
 use App\Util\UtilTime;
 use Exception;
 use DB;
+use Log;
 
 class TransTicketRepository extends BaseRepository
 {
-    
     public function getTickets(Int $offset, Int $limit, String $filter=null, String $start_date=null, String $end_date=null)
     {
         $tickets = AccunixCouponChildLogs::select('project_burgerking_coupon_child_logs.created_at',
@@ -31,12 +33,75 @@ class TransTicketRepository extends BaseRepository
                 ->orWhere('project_burgerking_coupon_child_logs.data->transfer->target_user', 'LIKE', "{$filter}%");
         } else {
             $tickets = $tickets->whereNotNull('project_burgerking_coupon_child_logs.data->transfer');
-            // $tickets = $tickets->whereNotNull('project_burgerking_coupon_child_logs.data');
         }
 
         $total   = $tickets->count();
         $records = $tickets->offset($offset)->limit($limit)->get();
 
         return collect(compact('total', 'records'));
+    }
+
+    public function syncPBCC()
+    {
+        $coupons = AccunixCouponChildLogs::whereNotNull('data->transfer');
+        $last    = $this->getPBCCLast();
+
+        if (!is_null($last)) {
+            $coupons = $coupons->where('id', '>', $last->id);
+        }
+        $coupons = $coupons->get();
+        
+        if (empty($coupons)) {
+            return ;
+        }
+
+        $coupons->map(function($item) {
+            $data = $item->toArray();
+            
+            if (ProjectBurgerkingCouponChildLogs::create($data)) {
+                Log::info('project_burgerking_coupon_child_logs sync ok! ('.$data['id'].')');
+            } else {
+                Log::error('project_burgerking_coupon_child_logs sync failed! ('.$data['id'].')');
+            }
+        });
+
+        return $coupons;
+    }
+
+    public function syncPBC($coupons=null)
+    {
+        if (empty($coupons)) {
+            return ;
+        }
+
+        $coupons->map(function($item) {
+            $data = AccunixCoupons::where('guid', $item->coupon_guid)
+                    ->where('campaign_guid', $item->campaign_guid)->first()->toArray();
+
+            if (empty($data)) {
+                Log::info('can not find data from accunix project_burgerking_coupons! ('.$item->campaign_guid.'、'.$item->coupon_guid.')');
+            } else {
+                $id = $data['id'];
+                unset($data['id']);
+
+                if (ProjectBurgerkingCoupons::updateOrCreate(['id' => $id], $data)) {
+                    Log::info('project_burgerking_coupons sync ok! ('.$item->campaign_guid.'、'.$item->coupon_guid.')');
+                } else {
+                    Log::error('project_burgerking_coupons sync failed! ('.$item->campaign_guid.'、'.$item->coupon_guid.')');
+                }
+            }
+        });
+
+        return ;
+    }
+
+    private function getPBCLast()
+    {
+        return ProjectBurgerkingCoupons::latest()->first();
+    }
+
+    private function getPBCCLast()
+    {
+        return ProjectBurgerkingCouponChildLogs::latest()->first();
     }
 }
