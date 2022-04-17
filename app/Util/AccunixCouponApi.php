2622fc165f3d5f17fc67f767dfff1475d7d71d90
nick chen <>
2022-04-18 03:42:28 +0800

    精誠 accunix class

---
+++ app/Util/AccunixCouponApi.php
@@ -0,0 +1,162 @@
+<?php
+namespace App\Util;
+
+class AccunixCouponApi
+{
+    protected $baseUrl = 'https://api-tf.accunix.net/api/project/burger-king';
+    protected $log;
+    protected $AccessToken;
+    protected $headers;
+
+    //建構
+    public function __construct()
+    {
+        $this->log = base_path().'/private/storage/'.env('APP_NAME').'/log/accunix_api/coupon';
+        
+        if (!is_dir($this->log)) {
+            mkdir($this->log, 0777, true);
+        }
+
+        $this->headers = [
+            'Content-Type: application/json;'
+        ];
+    }
+    ##
+
+    //設定access token
+    public function setAccessToken(String $AccessToken)
+    {
+        $this->AccessToken = $AccessToken;
+        $this->setHeaders(["Authorization: Bearer {$AccessToken}"]);
+
+        return $this;
+    }
+
+    //設定額外檔頭
+    protected function setHeaders(Array $headers)
+    {
+        $this->headers = array_merge($this->headers, $headers);
+        return $this;
+    }
+    ##
+
+
+    public function couponChildrenList(String $token)
+    {
+        if (!empty($token)) {
+            $post_data['userToken'] = $token;
+        }
+
+        $this->url = "{$this->baseUrl}/coupon-children-list?".http_build_query($post_data);
+
+        return $this->send($post_data, 'GET');
+    }
+
+    public function check(String $code=NULL)
+    {
+        $this->url = "{$this->baseUrl}/check";
+
+        if (!empty($code)) {
+            $post_data['code'] = $code;
+        }
+
+        return $this->send($post_data);
+    }
+
+    protected function send(Array $post_data, String $method=NULL)
+    {
+        $method = empty($method) ? 'POST' : strtoupper($method);
+
+        //發出
+        $ch = curl_init($this->url);
+
+        if ($method != 'GET') {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+        }
+
+        if ($method == 'PATCH') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
+        }
+
+        if (!empty($this->headers)) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        }
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        try {
+            $json = curl_exec($ch);
+            $response = json_decode($json, true);
+            $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        } catch (Exception $e) {
+            $this->Logs($e->getMessage(), 'error', 'exception');
+
+            return [
+                'status'    => 402,
+                'message'   => '其他錯誤',
+            ];
+        }
+        curl_close($ch);
+        ##
+
+        //log
+        $log = "End-Point: {$this->url}({$method})\n";
+        $log .= 'Request: '.json_encode($post_data, JSON_UNESCAPED_UNICODE)."\n";
+        $log .= 'Response: '.json_encode($response, JSON_UNESCAPED_UNICODE)."\n";
+        $log .= 'Response Code: '.$status."\n";
+
+        $this->Logs($log, 'request');
+        ##
+
+        // $status = ($status == 200) ? 200 : 400;
+        $return = [
+            'status'    => $status,
+            'message'   => $response['message'] ?? 'success',
+        ];
+
+        if ($status == 200) {
+            if (isset($response['message'])) unset($response['message']);
+            if (!empty($response)) {
+                $return['data'] = $response;
+            }
+        }
+
+        return $return;
+    }
+
+    //資訊紀錄
+    protected function Logs($response, $path, $filename='')
+    {
+        // set log
+        $filename = $path ?? "";
+        $path = $path ?? $this->log;
+
+        $path = empty($filename) ? $this->log : $this->log.'/'.$filename;
+
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+        ##
+
+        $_SERVER['REQUEST_SCHEME'] = $_SERVER['REQUEST_SCHEME'] ?? '';
+        $_SERVER['SERVER_NAME'] = $_SERVER['SERVER_NAME'] ?? '';
+        $_SERVER['REQUEST_URI'] = $_SERVER['REQUEST_URI'] ?? '';
+        $_SERVER['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'] ?? '';
+        $_SERVER['REMOTE_HOST'] = $_SERVER['REMOTE_HOST'] ?? '';
+
+        $request_detail = "Remote_Addr: {$_SERVER['REMOTE_ADDR']}\n";
+        $request_detail .= "Remote_Host: {$_SERVER['REMOTE_HOST']}\n";
+        $request_detail .= "End-Point: ".$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']."\n";
+
+        file_put_contents(
+            $path.'/'.$filename.'_'.date("Ymd").'.log',
+            date("Y-m-d H:i:s")."\n========================\n".
+            print_r($request_detail, true)."\n".
+            print_r($response, true)."\n".
+            "========================\n",
+            FILE_APPEND
+        );
+    }
+    ##
+}
---
+++ app/Util/SystexApi.php
@@ -0,0 +1,221 @@
+<?php
+namespace App\Util;
+
+class SystexApi
+{
+    protected $log;
+
+    public function __construct()
+    {
+        // set error log file path
+        $this->log = base_path().'/private/storage/'.env('APP_NAME').'/log/systex_api';
+
+        if (!is_dir($this->log)) {
+            mkdir($this->log, 0777, true);
+        }
+    }
+    ##
+
+    //curl
+    public function send($setHeader,$postData)
+    {
+        
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $setHeader);
+        // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+        // curl_setopt($ch, CURLOPT_TIMEOUT, 4);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt ($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_URL, env('SYSTEX_API'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        
+        // 紀錄開始執行時間
+        $timeStart = microtime(true);
+        
+        $result = curl_exec($ch);
+
+        // 紀錄結束執行時間
+        $timeEnd = microtime(true);
+
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        //log
+        $log = "End-Point: ".env('SYSTEX_API')."\n";
+        $log .= 'Request: '.$postData."\n";
+        $log .= 'Response: '.$result."\n";
+        $log .= 'Response Code: '.$httpcode."\n";
+
+        // 紀錄打api的執行時間
+        $log .= 'Response Time: '.number_format($timeEnd - $timeStart, 3)." seconds\n";
+
+        $this->Logs($log, 'request');
+
+        return json_decode($result,true);
+    }
+
+    //簽到交易
+    public function SignOn()
+    {
+         $requst_body =json_encode([
+            "EndPoint" => "SignOn",
+            "DateTime" => date('ymdHis'),
+            "SignOnType" => 2,
+            "SignOnDate" => date('Ymd'),
+            "Mid" => "233091780000997",
+            "Tid" => "00099701"
+         ]);
+
+        $result = md5(env('SYSTEX_CHECK_PREFIX').$requst_body.'SYSTEX');
+        $cbc = openssl_encrypt($result, 'DES-CBC',env('SYSTEX_CHECK_PASSPHRASE'),0,env('SYSTEX_CHECK_IV')); // 0 = PKCS7 + base64_encode
+        $header = ["Mask: ".substr($cbc,0,4).$result.substr($cbc, -4)];
+
+        return $this->send($header,$requst_body);        
+    }
+    ##
+
+    //CheckSum
+    public function CheckSum(array $data)
+    {
+
+        $str = $data['Mid'].$data['Tid'].$data['OrderId'].$data['DateTime'];
+    
+        //補0到8的倍數
+        $blocksize=8;
+        $pad = $blocksize - (strlen($str) % $blocksize);
+        $str = $str . str_repeat("\0", $pad);
+
+        /*
+            options = 0 : 默認 pkcs7 padding + base64
+            options = 1 : OPENSSL_RAW_DATA ， pkcs7 padding
+            options = 2 : OPENSSL_ZERO_PADDING ， 需先補0 不自動補 + base64
+            options = 3 : OPENSSL_NO_PADDING
+
+            DES-EDE   key長度 = 16
+            DES-EDE3  key長度 = 24
+        */
+
+        $res = $this->SignOn();
+
+        return openssl_encrypt($str, 'DES-EDE', $res['WorkKey'],2,'');
+    }
+
+    public function QueryBonus(String $QRCard)
+    {
+        $data =[
+            "Host" => "SVC",
+            "EndPoint" => "QueryBonus",
+            "SignOnType" => "2",
+            "DateTime" => date('ymdHis'),
+            "Mid" => "233091780000997",
+            "Tid" => "00099701",
+            "OrderId" => "5001000120210205013821",
+            "QRCard" => $QRCard
+
+        ];
+
+        $header = ["CheckSum: ".$this->CheckSum($data)];
+        $body=json_encode($data);
+
+        return $this->send($header,$body);
+    }
+
+    public function QueryTxn(String $QRCard)
+    {
+        $data =[
+            "Host" => "SVC",
+            "EndPoint" => "QueryTxn",
+            "SignOnType" => "2",
+            "DateTime" => date('ymdHis'),
+            "Mid" => "233091780000997",
+            "Tid" => "00099701",
+            "OrderId" => "5001000120210205013821",
+            "QRCard" => $QRCard
+
+        ];
+
+        $header = ["CheckSum: ".$this->CheckSum($data)];
+        $body=json_encode($data);
+
+        return $this->send($header,$body);
+    }
+
+    public function AdjustPointMinus(string $OrderId, string $storedCardNo, string $amount)
+    {
+        $data =[
+            "Host" => "SVC",
+            "EndPoint" => "AdjustPointMinus",  
+            "SignOnType" =>"2",
+            "DateTime" => date('ymdHis'),
+            "Mid" => "233091780000997",
+            "Tid" => "00099701",
+            "OrderId" => $OrderId,
+            //"MerchOrderNo" => $merchOrderNo,
+            "SvcCardNo" => $storedCardNo,
+            "CouponList" =>[         
+                [
+                    "BonusId" => "",
+                    "Amount" => $amount
+                ]
+            ]
+        ];
+
+
+        $header = ["CheckSum: ".$this->CheckSum($data)];
+        $body=json_encode($data);
+
+        return $this->send($header,$body);
+    }
+
+    public function AdjustPointPlus(string $OrderId, string $storedCardNo, string $merchOrderNo, string $amount)
+    {
+        $data =[
+            "Host" => "SVC",
+            "EndPoint" => "AdjustPointPlus",  
+            "SignOnType" =>"2",
+            "DateTime" => date('ymdHis'),
+            "Mid" => "233091780000997",
+            "Tid" => "00099701",
+            "OrderId" => $OrderId,
+            //"MerchOrderNo" => $merchOrderNo,
+            "SvcCardNo" => $storedCardNo,
+            "CouponList" =>[         
+                [
+                    "BonusId" => "",
+                    "Amount" => $amount,
+                    "StartDate" => "00010101",
+                    "EndDate" => "20221231"
+                ]
+            ]
+        ];
+          
+        $header = ["CheckSum: ".$this->CheckSum($data)];
+        $body=json_encode($data);
+
+        return $this->send($header,$body);
+    }
+
+    //資訊紀錄
+    protected function Logs($response, $filename) {
+        $_SERVER['REQUEST_SCHEME'] = $_SERVER['REQUEST_SCHEME'] ?? '';
+        $_SERVER['SERVER_NAME'] = $_SERVER['SERVER_NAME'] ?? '';
+        $_SERVER['REQUEST_URI'] = $_SERVER['REQUEST_URI'] ?? '';
+        $_SERVER['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'] ?? '';
+        $_SERVER['REMOTE_HOST'] = $_SERVER['REMOTE_HOST'] ?? '';
+
+        $request_detail = "Remote_Addr: {$_SERVER['REMOTE_ADDR']}\n";
+        $request_detail .= "Remote_Host: {$_SERVER['REMOTE_HOST']}\n";
+        $request_detail .= "End-Point: ".$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']."\n";
+        
+        file_put_contents(
+            $this->log . '/' . $filename.'_'.date("Ymd").'.log',
+            date("Y-m-d H:i:s")."\n========================\n".
+                print_r($request_detail, true)."\n".
+                print_r($response, true)."\n".
+                "========================\n",
+            FILE_APPEND
+        );
+    }
+    ##
+}
