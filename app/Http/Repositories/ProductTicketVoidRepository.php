52b1616e1b502c64b86ae698bf1a8b66c75d90dc
ellen <ellen@accuhit.net>
2022-04-29 10:30:13 +0800

    更改return value

--- app/Http/Repositories/ProductTicketVoidRepository.php
+++ app/Http/Repositories/ProductTicketVoidRepository.php
@@ -8,21 +8,19 @@ class ProductTicketVoidRepository extends BaseRepository
 {
     public function setVoid(array $voidData)
     {
-        try{
-            $result = ProductTicketVoid::create([
-                'mobile' => $voidData['mobile'],
-                'source_system' => '5',
-                'order_id' => $voidData['order_id'],
-                'old_order_id' => $voidData['old_order_id'],
-                'product_id' => $voidData['product_id'],
-                'voidnum' => $voidData['voidnum'],
-                'voidamount' => $voidData['voidamount'],
-                'tender_name' => 'CMS',
-                'remark' => $voidData['remark']
-            ]);
-            return empty($result) ? false : true;
-        }catch (\Exception $e){
-            throw new \Exception($e->getMessage());
-        }
+
+        $result = ProductTicketVoid::create([
+            'mobile' => $voidData['mobile'],
+            'source_system' => '5',
+            'order_id' => $voidData['order_id'],
+            'old_order_id' => $voidData['old_order_id'],
+            'product_id' => $voidData['product_id'],
+            'voidnum' => $voidData['voidnum'],
+            'voidamount' => $voidData['voidamount'],
+            'tender_name' => 'CMS',
+            'remark' => $voidData['remark']
+        ]);
+        return empty($result) ? false : true;
+
     }
 }
